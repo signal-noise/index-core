abfcca281cf9be2096758c095b709d5b383afc3b
Tom Pearson <tompearson@signalnoise.io>
2021-09-30 14:24:25 +0100

    add linting and fix linting errors

--- src/index-core.js
+++ src/index-core.js
@@ -1,22 +1,51 @@
-import { calculateWeightedMean, clone } from './utils.js'
+import { calculateWeightedMean, clone } from './utils.js';
 
-function indexer(indicatorsData=[], entities=[], indexMax = 100){
-  if(indicatorsData.length==0||entities.length==0) return {};
+function indexer(indicatorsData = [], entities = [], indexMax = 100) {
+  if (indicatorsData.length === 0 || entities.length === 0) return {};
   const indicatorLookup = Object.fromEntries( // a look up for indicators
     indicatorsData
-      .map(indicator=>([indicator.id, indicator]))
-  ); 
+      .map((indicator) => ([indicator.id, indicator])),
+  );
   const indexedData = {};
 
-  calculateIndex();
-
-  function getEntity(name){
-    return entities.find(d=>d.name==name);
+  function getEntity(name) {
+    return entities.find((d) => d.name === name);
   }
 
-  function adjustValue(name, indicatorID, value){
-    let e = getEntity(name);
-    if(indicatorLookup[indicatorID].type=='calculated'){
+  function indexEntity(entity, calculationList) {
+    const newEntity = clone(entity);
+    // indicators are all the properties that start with a digit
+    // make a lookup for the entities values
+    const indexScore = {};
+    // get a list of the values we need to calculate
+    // in order of deepest in the heirachy to to shallowist
+    calculationList.forEach((i) => {
+      if (newEntity[i]) { console.log('overwriting', i); }
+      const componentIndicators = indicatorsData
+        .filter((j) => (j.id.indexOf(i) === 0 && j.id.length === i.length + 2))
+        .map((indicator) => ({
+          id: indicator.id,
+          value: Number(newEntity[indicator.id]),
+          weight: indicator.userWeighting
+            ? Number(indicator.userWeighting)
+            : Number(indicator.weighting),
+          invert: !!indicator.invert,
+          range: [
+            indicator.min ? Number(indicator.min) : 0,
+            indicator.max ? Number(indicator.max) : indexMax,
+          ],
+        }));
+      // calculate the weighted mean of the component indicators on the newEntity
+      // assign that value to the newEntity
+      newEntity[i] = calculateWeightedMean(componentIndicators, indexMax);
+      return newEntity;
+    });
+    return indexScore;
+  }
+
+  function adjustValue(name, indicatorID, value) {
+    const e = getEntity(name);
+    if (indicatorLookup[indicatorID].type === 'calculated') {
       console.warn(`${indicatorID} is a calculated value and can not be adjusted directly, perhaps you meant to adjust the weighting?`);
       return clone(e);
     }
@@ -25,70 +54,35 @@ function indexer(indicatorsData=[], entities=[], indexMax = 100){
     return indexEntity(Object.assign(clone(e), e.user));
   }
 
-  function adjustWeight(indicatorID, weight){
-    // TODO: allow group (,subgroup etc.) weighting adjustment
-    // TODO: make the index recalculating take into account what 
-    //    has changed in the data rather than doing the whole shebang
-    indicatorLookup[indicatorID].userWeighting = weight;
-    calculateIndex();
-  }
-
-  function indexEntity(entity, calculationList){
-    // indicators are all the properties that start with a digit
-    // make a lookup for the entities values
-    const indexScore = {};
-    //get a list of the values we need to calculate 
-    // in order of deepest in the heirachy to to shallowist
-    console.log('---', calculationList);
-    calculationList.forEach(i => {
-      if(entity[i]){ console.log('overwriting', i) }
-      const componentIndicators = indicatorsData
-        .filter(j => (j.id.indexOf(i) === 0 && j.id.length === i.length+2))
-        .map(indicator=>{
-          return {
-            id: indicator.id,
-            value: Number(entity[indicator.id]),
-            weight: indicator.userWeighting 
-              ? Number(indicator.userWeighting) 
-              : Number(indicator.weighting),
-            invert: indicator.invert 
-              ? true 
-              : false,
-            range: [
-              indicator.min ? Number(indicator.min) : 0 , 
-              indicator.max ? Number(indicator.max) : indexMax
-            ]
-          }
-        });
-      // calculate the weighted mean of the component indicators on the _entity_
-      // assign that value to the _entity_
-      entity[i] = calculateWeightedMean(componentIndicators, indexMax);
-      console.log(i, entity[i]);
-    });
-    return indexScore;
-  }
-
-  function calculateIndex(){
+  function calculateIndex() {
     // TODO: allow indicators to be excluded fomr the calculation list
     const calculationList = indicatorsData
-      .filter(i=>i.id.match(/^\d/) && i.type=='calculated')
-      .map(i=>i.id)
-      .sort((i1, i2)=>{
-        return (i2.length - i1.length);
-      });
+      .filter((i) => i.id.match(/^\d/) && i.type === 'calculated')
+      .map((i) => i.id)
+      .sort((i1, i2) => (i2.length - i1.length));
 
-    entities.forEach(entity => {
+    entities.forEach((entity) => {
       const indexedEntity = indexEntity(entity, calculationList);
       indexedEntity.data = entity;
       indexedData[entity.name] = indexedEntity;
     });
   }
 
+  function adjustWeight(indicatorID, weight) {
+    // TODO: allow group (,subgroup etc.) weighting adjustment
+    // TODO: make the index recalculating take into account what
+    //    has changed in the data rather than doing the whole shebang
+    indicatorLookup[indicatorID].userWeighting = weight;
+    calculateIndex();
+  }
+
+  calculateIndex();
+
   return {
     getEntity,
     adjustWeight,
     adjustValue,
-    indexedData
+    indexedData,
   };
 }
 
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,23 @@
-
-export function clone(o){
+export function clone(o) {
   return JSON.parse(JSON.stringify(o));
 }
 
-export function normalise(value, range=[0,100], normaliseTo=100){
-  return ((value - range[0]) / (range[1]-range[0]))* normaliseTo;
+export function normalise(value, range = [0, 100], normaliseTo = 100) {
+  return ((value - range[0]) / (range[1] - range[0])) * normaliseTo;
 }
 
-export function calculateWeightedMean(weightedValues, normaliseTo = 100){
+export function calculateWeightedMean(weightedValues, normaliseTo = 100) {
   let weightedSum = 0;
   let cumulativeWeight = 0;
-  for(let i = 0; i<weightedValues.length; i++){
-    const normalisedValue = normalise(weightedValues[i].value, weightedValues[i].range, normaliseTo);
-    const weightedValue = weightedValues[i].invert 
-      ? ((normaliseTo - normalisedValue) * weightedValues[i].weight)
-      : (normalisedValue * weightedValues[i].weight);
+  for (let i = 0; i < weightedValues.length; i += 1) {
+    const indicator = weightedValues[i];
+    const normalisedValue = normalise(indicator.value, indicator.range, normaliseTo);
+    const weightedValue = indicator.invert
+      ? ((normaliseTo - normalisedValue) * indicator.weight)
+      : (normalisedValue * indicator.weight);
 
-    weightedSum = weightedSum + weightedValue;
-    cumulativeWeight = cumulativeWeight + weightedValues[i].weight
+    weightedSum += weightedValue;
+    cumulativeWeight += indicator.weight;
   }
 
   return weightedSum / cumulativeWeight;
