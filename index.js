21e8c9e668b9b1bcf2c252750cd83c7d424ebcc8
Tom Pearson <tompearson@signalnoise.io>
2021-11-30 16:06:10 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,12 @@ import fs from 'fs';
 import indexCore from './src/index-core.js';
 
 const waterRootDir = 'data/wateroptimisation';
-const educationRootDir = 'data/education';
 
 const waterIndicators = csvParse(fs.readFileSync(`${waterRootDir}/indicators.csv`, 'utf-8'));
 const waterEntities = csvParse(fs.readFileSync(`${waterRootDir}/entities.csv`, 'utf-8'));
 
-const educationIndicators = csvParse(fs.readFileSync(`${educationRootDir}/indicators.csv`, 'utf-8'));
-const educationEntities = csvParse(fs.readFileSync(`${educationRootDir}/entities.csv`, 'utf-8'));
-
 const waterOptimisationIndex = indexCore(waterIndicators, waterEntities);
 
-
-const educationIndex = indexCore(educationIndicators, educationEntities);
 
 // console.log(waterOptimisationIndex.indexedData['Abu Dhabi']['1']);
 // console.log(waterOptimisationIndex.indexedData['Abu Dhabi'].value);
@@ -22,6 +16,10 @@ const educationIndex = indexCore(educationIndicators, educationEntities);
 // console.log(waterOptimisationIndex.getIndexMean('1.1'))
 // console.log(waterOptimisationIndex.getIndexMean('2.1.1'))
 // console.log(waterOptimisationIndex.getIndexMean())
+console.log(waterOptimisationIndex.indexedData);
 
-console.log(educationIndex.indexStructure);
-console.log(educationIndex.indexedData);
+waterOptimisationIndex.filterIndicators(indicator=>{
+  return String(indicator.id).indexOf('b')>0;
+})
+//console.log(waterOptimisationIndex.indexStructure);
+console.log(waterOptimisationIndex.indexedData);
