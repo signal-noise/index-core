318164dd0b8466424981a74d51710547d69767ae
Tom Pearson <tompearson@signalnoise.io>
2022-03-30 19:47:27 +0100

    add clamping and tests to the utils

--- src/utils.js
+++ src/utils.js
@@ -2,16 +2,24 @@ export function clone(o) {
   return JSON.parse(JSON.stringify(o));
 }
 
-export function normalise(value, range = [0, 100], normaliseTo = 100) {
-  return ((value - range[0]) / (range[1] - range[0])) * normaliseTo;
+export function clamper(range, value){ // restrict a value to between the vales of a tuple
+  return Math.min(Math.max(value,range[0]), range[1]);
 }
 
-export function calculateWeightedMean(weightedValues, normaliseTo = 100) {
+export function normalise(value, range = [0, 100], normaliseTo = 100, clamp = false) {
+  let x = value;
+  if(clamp){
+    x = clamper(range, value);
+  }
+  return ((x - range[0]) / (range[1] - range[0])) * normaliseTo;
+}
+
+export function calculateWeightedMean(weightedValues, normaliseTo = 100, clamp = false) {
   let weightedSum = 0;
   let cumulativeWeight = 0;
   for (let i = 0; i < weightedValues.length; i += 1) {
     const indicator = weightedValues[i];
-    const normalisedValue = normalise(indicator.value, indicator.range, normaliseTo);
+    const normalisedValue = normalise(indicator.value, indicator.range, normaliseTo, clamp);
     const weightedValue = indicator.invert
       ? ((normaliseTo - normalisedValue) * indicator.weight)
       : (normalisedValue * indicator.weight);
