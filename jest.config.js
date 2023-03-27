39e75cbdd754f5bb4e4a02a167a5bc46f28565da
Kelly Monson <kellymonson@signalnoise.io>
2023-03-27 17:12:39 +0200

    Module imports work now for jest

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,21 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: [
-    '<rootDir>',
-  ],
-  modulePaths: [
-    '<rootDir>',
-  ],
   moduleDirectories: [
     'node_modules',
   ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
